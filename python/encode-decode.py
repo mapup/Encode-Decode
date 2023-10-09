b73094b73418bf6723988c974ecb6fff39f25f8f
Deepak Karthik <77111326+DeepakTG7@users.noreply.github.com>
2023-10-09 23:39:10 +0530

    Adding reference url in the comments

--- python/encode-decode.py
+++ python/encode-decode.py
@@ -6,8 +6,8 @@ def encode(coords):
 def decode(encoded):
     return polyline.decode(encoded, 5)
 
-encoded = encode([(37.7749, -122.4194), (34.0522, -118.2437)]) #Refer to encoded/decoded polylines from the sample-polylines folder
+encoded = encode([(37.7749, -122.4194), (34.0522, -118.2437)]) #Refer to encoded/decoded polylines from https://github.com/mapup/Encode-Decode-Google-Polyline/tree/main/sample-polylines
 print("Encoded: ", encoded)
 
-decoded = decode(encoded) #Refer to encoded/decoded polylines from the sample-polylines folder
+decoded = decode(encoded) #Refer to encoded/decoded polylines from https://github.com/mapup/Encode-Decode-Google-Polyline/tree/main/sample-polylines
 print("Decoded: ", decoded)
